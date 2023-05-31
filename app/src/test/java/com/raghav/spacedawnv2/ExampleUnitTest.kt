faa2787b5924b49a208d22079b0e6cf85caec6d1
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-05-31 11:39:35 +0530

    add Ktlint and use Java 17 to build project with AGP 8.0.0 or after

--- app/src/test/java/com/raghav/spacedawnv2/ExampleUnitTest.kt
+++ app/src/test/java/com/raghav/spacedawnv2/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.raghav.spacedawnv2
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
