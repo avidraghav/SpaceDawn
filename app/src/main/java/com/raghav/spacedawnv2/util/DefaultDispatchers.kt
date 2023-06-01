39db94745bf6b6c6cb6822112ae92734c950379b
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-06-01 15:42:15 +0530

    delete interface DispatcherProvider

--- app/src/main/java/com/raghav/spacedawnv2/util/DefaultDispatchers.kt
+++
@@ -1,9 +0,0 @@
-package com.raghav.spacedawnv2.util
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-class DefaultDispatchers : DispatcherProvider {
-    override val main: CoroutineDispatcher
-        get() = Dispatchers.Main
-}
