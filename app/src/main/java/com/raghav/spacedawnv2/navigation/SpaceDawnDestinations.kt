7a1028fa4ddcacfc08a1ecb08a55253469eca281
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-07-20 17:27:46 +0530

    refactor MainActivity code

--- app/src/main/java/com/raghav/spacedawnv2/navigation/SpaceDawnDestinations.kt
+++ app/src/main/java/com/raghav/spacedawnv2/navigation/SpaceDawnDestinations.kt
@@ -1,21 +1,27 @@
 package com.raghav.spacedawnv2.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.ui.graphics.vector.ImageVector
+
 /**
  * Available destinations in the app
  */
 interface Destination {
     val route: String
     val label: String
+    val icon: ImageVector
 }
 
 object LaunchesScreen : Destination {
     override val route = "launches_list"
     override val label = "Launches"
+    override val icon = Icons.Default.Menu
 }
 
 object RemindersScreen : Destination {
     override val route = "reminders_list"
     override val label = "Reminders"
+    override val icon = Icons.Default.Notifications
 }
-
-val navigationBarScreens = listOf(LaunchesScreen, RemindersScreen)
