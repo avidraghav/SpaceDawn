dea6d4e770697245bf38a2d2afeab60096cb2d12
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-09-19 16:29:34 +0530

    resolve merge conflicts and fixes

--- buildSrc/src/main/java/HiltPlugin.kt
+++ buildSrc/src/main/java/HiltPlugin.kt
@@ -5,7 +5,8 @@ class HiltPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         target.plugins.apply("com.google.dagger.hilt.android")
-        target.dependencies.add("implementation", "com.google.dagger:hilt-android:2.44")
-        target.dependencies.add("kapt", "com.google.dagger:hilt-compiler:2.44")
+        target.plugins.apply("kotlin-kapt")
+        target.dependencies.add("implementation", "com.google.dagger:hilt-android:2.47")
+        target.dependencies.add("kapt", "com.google.dagger:hilt-compiler:2.47")
     }
 }
