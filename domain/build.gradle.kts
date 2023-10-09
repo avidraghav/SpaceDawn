5d8ec6ba4206fbe039374a47020385e9bab2633d
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-10-09 12:38:48 +0530

    use Kotlin based Gradle Scripts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,8 +1,10 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
-    id 'java-library'
-    id 'org.jetbrains.kotlin.jvm'
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
     id("com.google.devtools.ksp")
-    id "org.jlleitschuh.gradle.ktlint"
+    id("org.jlleitschuh.gradle.ktlint")
 }
 
 java {
@@ -11,27 +13,26 @@ java {
 }
 
 ktlint {
-    android = true
-    ignoreFailures = false
+    android.set(false)
+    ignoreFailures.set(false)
     reporters {
-        reporter "plain"
-        reporter "checkstyle"
-        reporter "sarif"
+        reporter(ReporterType.PLAIN)
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.SARIF)
     }
     filter {
         exclude {
-            exclude { it.file.path.contains("$buildDir/generated/") }
+            it.file.path.contains("$buildDir/generated/")
         }
     }
-    disabledRules = ["import-ordering", "max-line-length", "argument-list-wrapping"]
 }
 
 dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("javax.inject:javax.inject:1")
 
-    //Room
-    implementation "androidx.room:room-common:2.5.1"
+    // Room
+    implementation("androidx.room:room-common:2.5.1")
 
     // Moshi
     implementation("com.squareup.moshi:moshi:1.14.0")
@@ -40,8 +41,8 @@ dependencies {
     // Coroutines Flow
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 
-    def okhttp_interceptor = "5.0.0-alpha.2"
-    api "com.squareup.okhttp3:logging-interceptor:$okhttp_interceptor"
+    val okhttp_interceptor = "5.0.0-alpha.2"
+    api("com.squareup.okhttp3:logging-interceptor:$okhttp_interceptor")
 
     // For Making Kotlin Collections Immutable as to be considered stable
     api("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
